c0cae88b803cfdba3a0fe533cecbfacf00ad7200
Eliza Śpionek <test-git@youcode.pl>
2021-04-29 17:55:58 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("Hello everyone !");
+
+let changeQuotationButton = document.querySelector(".changeQuotationButton");
+let quotation = document.querySelector(".quotation")
+let changeColorButton = document.querySelector(".changeColorButton");
+
+
+
+let quotationDisnay = quotation.innerHTML;
+let quotationLarson = "<i>Poświęcaj tyle czasu na ulepszanie siebie,<br> byś nie miał go na krytykę innych.</i><br> <p>- Christian D. Larson</p>";
+
+changeQuotationButton.addEventListener("click", () => {
+    quotation.innerHTML = quotation.innerHTML === quotationDisnay ? quotationLarson : quotationDisnay;
+});
+
+changeColorButton.addEventListener("click", () => {
+    quotation.classList.toggle("newQuotation");
+});
+
+changeColorButton.addEventListener("click", () => {
+    changeColorButton.innerText = changeColorButton.innerText === "Zmień kolor cytatu" ? "Powrót do czarnego" : "Zmień kolor cytatu";
+
+});
+
+
