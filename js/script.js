634fc4501c4c517109e2201a20409d586d115e21
Eliza Śpionek <eliza.spionek@gmail.com>
2021-05-05 01:03:48 +0200

    scrip changed

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
-console.log("Hello everyone !");
+{
+    const welcome = () => {
+        console.log("Hello everyone !");
+    };
+    const changeQuotation = () => {
+        const quotation = document.querySelector(".js-quotation")
+        const quotationDisnay = quotation.innerHTML;
+        const quotationLarson = "<i>Poświęcaj tyle czasu na ulepszanie siebie,<br> byś nie miał go na krytykę innych.</i><br> <p>- Christian D. Larson</p>";
 
-let changeQuotationButton = document.querySelector(".js-changeQuotationButton");
-let quotation = document.querySelector(".js-quotation")
-let changeColorButton = document.querySelector(".js-changeColorButton");
+        quotation.innerHTML = quotation.innerHTML === quotationDisnay ? quotationLarson : quotationDisnay;
+    };
+    const changeQuotationColor = () => {
+        const quotation = document.querySelector(".js-quotation")
+        quotation.classList.toggle("aside__button--color");
+        changeColorButton.innerText = changeColorButton.innerText === "Zmień kolor cytatu" ? "Powrót do czarnego" : "Zmień kolor cytatu";
 
-let quotationDisnay = quotation.innerHTML;
-let quotationLarson = "<i>Poświęcaj tyle czasu na ulepszanie siebie,<br> byś nie miał go na krytykę innych.</i><br> <p>- Christian D. Larson</p>";
+    }
+    const init = () => {
+        const changeQuotationButton = document.querySelector(".js-changeQuotationButton");
+        changeQuotationButton.addEventListener("click", changeQuotation);
+        const changeColorButton = document.querySelector(".js-changeColorButton");
+        changeColorButton.addEventListener("click", changeQuotationColor);
+        welcome();
+    }
 
-changeQuotationButton.addEventListener("click", () => {
-    quotation.innerHTML = quotation.innerHTML === quotationDisnay ? quotationLarson : quotationDisnay;
-});
+    init();
 
-changeColorButton.addEventListener("click", () => {
-    quotation.classList.toggle("aside__button--color");
-});
-
-changeColorButton.addEventListener("click", () => {
-    changeColorButton.innerText = changeColorButton.innerText === "Zmień kolor cytatu" ? "Powrót do czarnego" : "Zmień kolor cytatu";
-
-});
-
-
+}
